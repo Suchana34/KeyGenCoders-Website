d59083fa1ee7f335258d50a37785a6a9a7706ab2
suchana <suchanachakraborty@gmail.com>
2020-03-07 01:09:39 +0530

    finished aboutpage

---
+++ src/Pages/about.js
@@ -0,0 +1,15 @@
+import React from "react";
+import Body from "../Components/Aboutpage/body";
+
+
+class About extends React.Component {
+  render() {
+    return (
+      <div>
+      <Body />
+      </div>
+    );
+  }
+}
+
+export default About;
