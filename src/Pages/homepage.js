a8a716fff6417bc744ba9d0018826cdcc6930745
suchana <suchanachakraborty@gmail.com>
2020-03-06 17:34:03 +0530

    various pages

---
+++ src/Pages/homepage.js
@@ -0,0 +1,17 @@
+import React from "react";
+import Body from "../Components/Homepage/body";
+import Header from "../Components/Homepage/header";
+
+
+class Home extends React.Component {
+  render() {
+    return (
+      <div>
+      <Body />
+      <Header />
+      </div>
+    );
+  }
+}
+
+export default Home;
