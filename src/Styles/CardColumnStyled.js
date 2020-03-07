bc4dd7f5036637a1b4d253fe1793938b846cd58e
suchana <suchanachakraborty@gmail.com>
2020-03-08 02:56:23 +0530

    styles for CardColumn

---
+++ src/Styles/CardColumnStyled.js
@@ -0,0 +1,13 @@
+import { CardColumns } from "react-bootstrap";
+import styled from "styled-components";
+
+const StyledCardColumns = styled(CardColumns)
+`
+margin-left: 13%;
+margin-right:10%;
+border: none;
+padding: 2rem;
+justify-content: center;
+`;
+
+export default StyledCardColumns;
