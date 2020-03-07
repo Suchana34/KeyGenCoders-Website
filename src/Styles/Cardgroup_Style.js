1716e0d9db3caa335e8581314929e246a8a7fb3e
suchana <suchanachakraborty@gmail.com>
2020-03-08 02:56:41 +0530

    modified

--- src/Styles/Cardgroup_Style.js
+++ src/Styles/Cardgroup_Style.js
@@ -5,8 +5,8 @@ const StyledCardGroup = styled(CardGroup)
 `
   margin-left: 10%;
   margin-right: 10%;
-  margin-bottom: 5rem;
-  margin-top: 5rem;
+  margin-bottom: 10%;
+  margin-top: 4%;
   border: none;
   padding: 1em;
 `;
