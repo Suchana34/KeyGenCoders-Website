a201bda27e8593788d03816cfeb5d181fe3ee4d4
suchana <suchanachakraborty@gmail.com>
2020-04-07 23:49:23 +0530

    Updates

---
+++ precache-manifest.4d7e52d2ca189f2862a37ada65450dd7.js
@@ -0,0 +1,82 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "af655ddafb3441504f1ac966c63c9c11",
+    "url": "/KeyGEnCoders/index.html"
+  },
+  {
+    "revision": "3598428ae8a4a19642de",
+    "url": "/KeyGEnCoders/static/css/2.47e06e2e.chunk.css"
+  },
+  {
+    "revision": "77b7f21de0fe36473579",
+    "url": "/KeyGEnCoders/static/css/main.19107fd9.chunk.css"
+  },
+  {
+    "revision": "3598428ae8a4a19642de",
+    "url": "/KeyGEnCoders/static/js/2.d8db9797.chunk.js"
+  },
+  {
+    "revision": "bf23039b4e70a07c5aeb1b4010df3572",
+    "url": "/KeyGEnCoders/static/js/2.d8db9797.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "77b7f21de0fe36473579",
+    "url": "/KeyGEnCoders/static/js/main.661ef22d.chunk.js"
+  },
+  {
+    "revision": "3259f41d2b53fcaa32ad",
+    "url": "/KeyGEnCoders/static/js/runtime-main.ef8009ca.js"
+  },
+  {
+    "revision": "9dac07fec84cbcc6e891f193e0047e7f",
+    "url": "/KeyGEnCoders/static/media/Abhinav_Ghosh.9dac07fe.jpeg"
+  },
+  {
+    "revision": "19f7b8d9e5616d66f15ee10b00187166",
+    "url": "/KeyGEnCoders/static/media/Ankit_Kurmi.19f7b8d9.jpeg"
+  },
+  {
+    "revision": "59f253efe6a6599c1e5de538c16c5541",
+    "url": "/KeyGEnCoders/static/media/Ayushman_Bilas_Thakur.59f253ef.jpeg"
+  },
+  {
+    "revision": "2f1bd38898d8eedcbd92a17cd0356990",
+    "url": "/KeyGEnCoders/static/media/Ritankar_Paul.2f1bd388.jpeg"
+  },
+  {
+    "revision": "d28191baf18133a762db578e08e666e1",
+    "url": "/KeyGEnCoders/static/media/Suchana_Chakrabarti.d28191ba.jpg"
+  },
+  {
+    "revision": "201c6874139dcff452840057bcfa0a1c",
+    "url": "/KeyGEnCoders/static/media/krishna_bose.201c6874.jpg"
+  },
+  {
+    "revision": "82c2465f7ac63e655dc0d2a7601a2484",
+    "url": "/KeyGEnCoders/static/media/logo.82c2465f.jpg"
+  },
+  {
+    "revision": "e71bc3d744c7838554d85a49aa8607b1",
+    "url": "/KeyGEnCoders/static/media/mayukh_Sengupta.e71bc3d7.jpg"
+  },
+  {
+    "revision": "96dada3ba8b959cfcd8233c982007784",
+    "url": "/KeyGEnCoders/static/media/saranya.96dada3b.jpeg"
+  },
+  {
+    "revision": "53971a13b42f8e906cff4dc7a01d1987",
+    "url": "/KeyGEnCoders/static/media/sayanti.53971a13.jpeg"
+  },
+  {
+    "revision": "8cbdbe4b825f72b531817eeb6469d3da",
+    "url": "/KeyGEnCoders/static/media/sourabh_jha.8cbdbe4b.jpeg"
+  },
+  {
+    "revision": "d19116953c7b0620ac0eb122a74723ae",
+    "url": "/KeyGEnCoders/static/media/sujan_dutta.d1911695.jpg"
+  },
+  {
+    "revision": "46d9f22baccdf7718ee608f0b5503c6c",
+    "url": "/KeyGEnCoders/static/media/undraw_coding_6mjf.46d9f22b.svg"
+  }
+]);
